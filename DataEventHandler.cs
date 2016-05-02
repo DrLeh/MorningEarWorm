3cdf6e8c3aeae7a4e34fd21510407827e75458a8
DrLeh <lehman.devon@gmail.com>
2016-05-02 16:33:19 -0400

    Initial Commit

---
+++ DataEventHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DLeh.Util
+{
+    /// <summary>
+    /// Allows easier transfer of data through events.
+    /// </summary>
+    /// <typeparam name="T">Type of data being sent in args</typeparam>
+    /// <param name="sender">sender of the event</param>
+    /// <param name="e">args containing the datatype T</param>
+    public delegate void DataEventHandler<T>(object sender, DataEventArgs<T> e);
+
+    /// <summary>
+    /// Event args that hold a data type for easier access.
+    /// </summary>
+    public class DataEventArgs<T> : EventArgs
+    {
+        public T Data { get; set; }
+
+        public DataEventArgs(T obj)
+        {
+            Data = obj;
+        }
+
+        public static implicit operator T(DataEventArgs<T> args)
+        {
+            return args.Data;
+        }
+        public static implicit operator DataEventArgs<T>(T t)
+        {
+            return new DataEventArgs<T>(t);
+        }
+        //public static implicit operator DataEventArgs<T>(T t) => DataEventArgs<T>(t);
+        //public static implicit operator T(DataEventArgs<T> args) => args.Data;
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,7 @@
+﻿#MorningEarWorm
+
+I have a tendency to wake up with songs in my head. Some of those songs seemed to be ones I felt that I hadn't even heard in a long time. I wrote this app to scrape my LastFM history to see when I had last heard that song. I was surprised when I saw that the song I woke up with in my head the day I wrote the app I hadn't listened to in two years, which I found very interesting.
+
+I want to turn this into a twitter bot and possibly a phone app, so I can wake up and just enter the song name and artist that I woke up with in my head, and click a button and it would tweet something like:
+
+> Today I woke up with Artist - Album in my head. It's been 415 days since I last heard this song!
