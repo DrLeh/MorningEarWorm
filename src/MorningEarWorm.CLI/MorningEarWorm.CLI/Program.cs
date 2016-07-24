5a86f1b57c9f564c3777883edf357c970aca519c
Devon Lehman <lehman.devon@gmail.com>
2016-07-24 15:54:12 -0400

    Reworked structure

---
+++ src/MorningEarWorm.CLI/MorningEarWorm.CLI/Program.cs
@@ -0,0 +1,40 @@
+﻿using MorningEarWorm.LastFM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorningEarWorm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Artist:");
+            var artist = Console.ReadLine();
+            Console.WriteLine("Track:");
+            var track = Console.ReadLine();
+
+            var client = new LastFMClient("thelehmanlip");
+            client.PageSearched += (sender, a) =>
+             {
+                 Console.WriteLine($"Searched page: {a.Data}");
+             };
+            var trackPlays = client.FindSongPlays(artist, track);
+            if (!trackPlays.Any())
+            {
+                Console.WriteLine("None found!");
+            }else
+            {
+                foreach (var trackPlay in trackPlays)
+                {
+                    Console.WriteLine($"Track found: {trackPlay}");
+                }
+            }
+            Console.WriteLine();
+            
+        }
+    }
+}
